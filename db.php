54930c5ba70878219e68ca139ac86a3cc5fa824a
nflorijan <florijan.nadj@gmail.com>
2021-03-15 20:35:56 +0100

    Zadatak 4

---
+++ db.php
@@ -0,0 +1,30 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "password";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+
+    function getDataFromDatabase($connection, $sql) {
+      $statement = $connection->prepare($sql);
+      $statement->execute();
+      $statement->setFetchMode(PDO::FETCH_ASSOC);
+      return $statement->fetchAll();
+    }
+
+    function getDataFromSinglePost($connection, $sql) {
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        return $statement->fetch();
+    }
+?>
